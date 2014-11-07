1532f62a9fa7e1a86b50f4f3732f03c997f77e96
Cyle Gage <cylegage@gmail.com>
2014-11-06 21:34:38 -0500

    working on interface and shit

--- browser/cylenet.js
+++ browser/cylenet.js
@@ -1,1 +1,24 @@
-document.getElementById('wat').innerHTML = 'haha';
+function id(wat) {
+	return document.getElementById(wat);
+}
+
+function init() {
+	id('go-btn').addEventListener('click', go, false);
+}
+
+function go() {
+	var crl = id('address-bar').value;
+	//alert(crl);
+	console.log('traveling to: ' + crl);
+}
+
+window.addEventListener("load", init, false);
+
+/*
+
+// rendering markdown:
+
+var markdown = require('markdown').markdown;
+document.write(markdown.toHTML("Hello **World**!"));
+
+*/
